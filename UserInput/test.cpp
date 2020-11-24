03362e2a24de4d5d8aa0f0762930e21c47311b5b
TheParenthesis <74931374+TheParenthesis@users.noreply.github.com>
2020-11-24 12:22:32 +0530

    I have created a Cpp file.
    This file contains a Cpp code for taking input from the user and display it on the same screen.

---
+++ UserInput/test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string a;
+
+    std::cout << "Enter a String: ";
+    cin >> a;
+    std::cout << "You Entered: " << a;
+    return 0;
+}
